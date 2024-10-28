ddc28c83766d6015bfd2ae89ed38faa2c8405688
Jakub Mamala <g00408184@atu.ie>
2024-10-28 14:47:50 +0000

    changed it into an interface and made it extend JpaRepository to Talk to a Database

--- src/main/java/com/example/demo/Repository/BookRepository.java
+++ src/main/java/com/example/demo/Repository/BookRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.Repository;
 
-public class BookRepository {
+import com.example.demo.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
