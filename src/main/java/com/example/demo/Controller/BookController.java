aa7dd7a5738fbbbbcf12f4271e751f88936baac3
Jakub Mamala <g00408184@atu.ie>
2024-10-28 15:00:35 +0000

    created get mapping

--- src/main/java/com/example/demo/Controller/BookController.java
+++ src/main/java/com/example/demo/Controller/BookController.java
@@ -1,4 +1,23 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Service.BookService;
+import com.example.demo.entity.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books") // Base URL for book-related APIs
 public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/get")
+    public ResponseEntity<Book> createBook(Book book) {
+        bookService.createBook(book);
+        return ResponseEntity.ok(book);
+    }
 }
