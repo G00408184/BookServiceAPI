ac5b5810e76618c37e2b731f341638314259599c
Jakub Mamala <g00408184@atu.ie>
2024-10-28 14:51:52 +0000

    created methods for Get and Add Books in Service

--- src/main/java/com/example/demo/Service/BookService.java
+++ src/main/java/com/example/demo/Service/BookService.java
@@ -1,4 +1,24 @@
 package com.example.demo.Service;
 
+import com.example.demo.Repository.BookRepository;
+import com.example.demo.entity.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookService {
+
+@Autowired
+private BookRepository bookRepository;
+
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
 }
