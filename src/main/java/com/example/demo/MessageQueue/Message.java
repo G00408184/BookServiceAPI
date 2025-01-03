13a06622aff0a41de85a126a3490075726131caa
Jakub Mamala <g00408184@atu.ie>
2025-01-03 15:22:00 +0000

    created a bunch of code to test out the queue updated pom

--- src/main/java/com/example/demo/MessageQueue/Message.java
+++ src/main/java/com/example/demo/MessageQueue/Message.java
@@ -1,4 +1,10 @@
 package com.example.demo.MessageQueue;
 
+import lombok.Data;
+
+@Data
 public class Message {
+    private String id;
+    private String content;
+    private String timestamp;
 }
--- src/main/java/com/example/demo/MessageQueue/MessageConsumer.java
+++ src/main/java/com/example/demo/MessageQueue/MessageConsumer.java
@@ -1,4 +1,14 @@
 package com.example.demo.MessageQueue;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MessageConsumer {
+
+    @RabbitListener(queues = "productQueue")
+    public void receiveMessage(Message message) {
+        System.out.println("Message received from queue: " + message);
+    }
 }
+
